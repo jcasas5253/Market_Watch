28895a7ee560b9ef3a37edab7647c7f8dde5fb79
Jacob Casas <skedabble@gmail.com>
2024-08-05 11:21:34 -0700

    Heroku

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/marketwatch')
+const MONGO_URI = 'mongodb+srv://jcasas5253:Drums4life!@cluster0.arafzxf.mongodb.net/myDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
